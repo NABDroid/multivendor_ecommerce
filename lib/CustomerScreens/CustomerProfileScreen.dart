766f8018aea5d831c693d0e629d3dc1c915b26b0
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2023-10-24 17:14:55 +0600

    added CustomerProfileScreen

--- lib/CustomerScreens/CustomerProfileScreen.dart
+++ lib/CustomerScreens/CustomerProfileScreen.dart
@@ -12,9 +12,111 @@ class _CustomerProfileScreenState extends State<CustomerProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Customer Profile", style: headingTextStyle,),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            CircleAvatar(
+              backgroundImage: AssetImage("assets/avatar.png"),
+              radius: 100,
+            ),
+            const SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Customer Name", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.location_on),
+                Text("Address",style: descriptionTextStyle,)
+              ],
+            ),
+            SizedBox(height: 30,),
+            SizedBox(
+              height: 2,
+              width: MediaQuery.sizeOf(context).width,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.person, size: 35,),
+                  SizedBox(width: 10,),
+                  Text("Personal Information", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+              width: MediaQuery.sizeOf(context).width,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.notifications, size: 35,),
+                  SizedBox(width: 10,),
+                  Text("Notifications", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+              width: MediaQuery.sizeOf(context).width,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.save, size: 35,),
+                  SizedBox(width: 10,),
+                  Text("Wishlist", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+              width: MediaQuery.sizeOf(context).width,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(Icons.shopping_bag, size: 35,),
+                  SizedBox(width: 10,),
+                  Text("Purchase History", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 2,
+              width: MediaQuery.sizeOf(context).width,
+              child: Container(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
